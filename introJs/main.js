11c932f94d66fd1fd397b075a5ef5e34a5f1dc76
hamdaouiwassim <wassim.hamdaoui@wi-mobi.com>
2021-10-06 13:33:42 +0200

    introjs

---
+++ introJs/main.js
@@ -0,0 +1,146 @@
+// cette fonction permet de afficher un message dans la console
+
+/*
+multi line comments
+*/
+
+
+console.log("hello world");
+
+// declaration des variables
+
+// type de donnes / Number (1 , 2.56) / Array / Object / String / Boolean (true,false) / undefined
+
+// var nom;
+// var prenom;
+// var age;
+
+
+
+
+// // Assignment -- initialisation --
+// nom = "Ahmed";
+// prenom = "ben romdhan";
+
+// //console.log("hello " +nom)
+// //console.log(age*3)
+
+// var existe = 0;
+
+// if (existe){ // if (existe == true) || ( existe == 1 )
+//     console.log("donnes existe");
+// }else{
+//     console.log("donnes n'existe plus");
+// }
+
+
+
+// var a = 5
+// var b = 6
+
+// // + , - , * , / , %
+
+// var c = b / 3
+
+// console.log(c)
+
+
+// console.log(++a)
+// console.log(a++)
+// console.log(a)
+
+// //a = a + 5; 
+// console.log(a+=5)
+
+
+
+/// Strings
+
+
+// ( var , let , const )
+
+// var nom ;//= "Ali";
+
+// let a = 12;
+// const pi = 3.14;
+// let a = 15;
+// console.log(a)
+// console.log(b)
+// Add()
+
+// function Add(){
+
+//     var nom = "mohamed";
+//     console.log(nom)
+// }
+
+let age = 20;
+let nom = "Ahmed";
+let prenom = 'ben romdhane';
+let nomprenom = `ahmed ben romdhane`;
+
+let para = "je suis a l'ecole";
+
+let paragraphe = `ahmed ${age}`;
+
+console.log(para.includes('sdfrrhdr'))
+
+// declaration des fonctions 
+
+function somme(a,b){
+    return a+b;
+
+}
+
+var s = somme(10,20)
+console.log(s)
+
+var cars = ["Volvo",["Audi A3","Audi A1"],"BMW"];
+cars[4] = "Toyota"
+cars.push("Golf")
+
+console.log(cars[1][0])
+
+
+const car = {type:"Fiat", model:"500", color:"white"};
+
+console.log(car)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
